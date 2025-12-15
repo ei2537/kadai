9af174434b5625611708398282b0b8b3df3841e4
mabuchi <mabuchi.ei@gmail.com>
2025-12-15 11:57:19 +0900

    jd

---
+++ prog/maxmin.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <hamako.h>
+
+int main () {
+    int a,b,max,min;
+    a = getint("input a: "); // 整数 a の入力
+    b = getint("input b: "); // 整数 b の入力
+    if(a>b){
+        max = a;
+        min = b;
+    }else{
+        max = b;
+        min = a;
+    }
+    printf("max = %d, min = %d\n",max,min);
+    return 0;
+}
